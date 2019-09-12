2afecafc76e5ee9e386f8232f436ecb3ce0d6c28
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-12 16:02:27 -0300

    smoke tests: checking if functions exist

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const search = () => {};
+export const searchAlbuns = () => {};
+export const searchArtists = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,1 +1,48 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai';
+import {
+  search,
+  searchAlbuns,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+} from '../src/main';
 
+describe('Spotify Wrappper', () => {
+  describe('Smoke tests', () => {
+    // search (genérico)
+    // searchAlbuns
+    // searchArtists
+    // searchTracks
+    // searchPlaylists
+
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+
+  describe('Generic Search', () => {
+    it('should call fetch function', () => {
+      // eslint-disable-next-line no-unused-vars
+      const artists = search();
+
+    });
+  });
+});
