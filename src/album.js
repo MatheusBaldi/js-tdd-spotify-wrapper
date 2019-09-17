703529a9aa6ce388514cafb3408d17c3e31a21ec
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-17 18:56:24 -0300

    Create functions to get album data

--- src/album.js
+++ src/album.js
@@ -1,5 +1,8 @@
-export const getAlbum = (id) => {
-  return fetch(`https://api.spotify.com/v1/albums/${id}`)
-    .then((data) => data.json());
-};
-export const getAlbumTracks = () => {};
+import { API_URL } from './config';
+import { toJSON } from './utils';
+
+export const getAlbum = (id) => fetch(`${API_URL}/albums/${id}`).then(toJSON);
+
+export const getAlbums = (ids) => fetch(`${API_URL}/albums/?ids=${ids}`).then(toJSON);
+
+export const getAlbumTracks = (id) => fetch(`${API_URL}/albums/${id}/tracks`).then(toJSON);
