6c28db2b53856268b63e8dc917f55e5e28a06e1c
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-20 16:59:25 -0300

    refactoring token usage

--- src/config.js
+++ src/config.js
@@ -1,2 +1,11 @@
-// eslint-disable-next-line import/prefer-default-export
+require('dotenv').config();
+
 export const API_URL = 'https://api.spotify.com/v1';
+export const HEADERS = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${process.env.SPOTIFY_AUTH_TOKEN}`,
+  },
+};
--- src/search.js
+++ src/search.js
@@ -1,18 +1,7 @@
-import { API_URL } from './config';
+import { API_URL, HEADERS } from './config';
 import { toJSON } from './utils';
 
-require('dotenv').config();
-
-// eslint-disable-next-line import/prefer-default-export
-export const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-    Authorization: `Bearer ${process.env.SPOTIFY_AUTH_TOKEN}`,
-  },
-}).then(toJSON);
-
+export const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`, HEADERS).then(toJSON);
 export const searchAlbums = (query) => search(query, 'album');
 export const searchArtists = (query) => search(query, 'artist');
 export const searchTracks = (query) => search(query, 'track');
